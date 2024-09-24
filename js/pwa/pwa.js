4882eaa942f1e6184b4f3227c2c61b61c66c4154
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-09-24 21:00:20 +0300

    Fixed PWA Name

--- js/pwa/pwa.js
+++ js/pwa/pwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/markdown_preview/js/pwa/service-worker.js')
+    navigator.serviceWorker.register('/markdown_previewer/js/pwa/service-worker.js')
       .then((registration) => {
         // console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
--- js/pwa/service-worker.js
+++ js/pwa/service-worker.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'markdown-preview-v1';
+const CACHE_NAME = 'markdown-previewer-v0.2';
 const urlsToCache = [
-  '/markdown_preview',
-  '/markdown_preview/index.html',
-  '/markdown_preview/css/global/style.css',
-  '/markdown_preview/css/global/scroll.css',
-  '/markdown_preview/css/global/colors.css',
-  '/markdown_preview/css/mdpreview/ghmd.min.css">',
-  '/markdown_preview/css/katex/katex.min.css">'
-  '/markdown_preview/imgs/check.png',
-  '/markdown_preview/imgs/copy.png',
-  '/markdown_preview/imgs/markdown.png',
-  '/markdown_preview/js/katex/katex.min.js',
-  '/markdown_preview/js/katex/auto-render.min.js',
-  '/markdown_preview/js/mdpreview/marked.min.js',
-  '/markdown_preview/js/mdpreview/preview.js',
-  '/markdown_preview/js/pwa/pwa.js',
-  '/markdown_preview/js/pwa/service-worker.js',
-  '/markdown_preview/manifest.json'
+  '/markdown_previewer',
+  '/markdown_previewer/index.html',
+  '/markdown_previewer/css/global/style.css',
+  '/markdown_previewer/css/global/scroll.css',
+  '/markdown_previewer/css/global/colors.css',
+  '/markdown_previewer/css/mdpreview/ghmd.min.css">',
+  '/markdown_previewer/css/katex/katex.min.css">',
+  '/markdown_previewer/imgs/check.png',
+  '/markdown_previewer/imgs/copy.png',
+  '/markdown_previewer/imgs/markdown.png',
+  '/markdown_previewer/js/katex/katex.min.js',
+  '/markdown_previewer/js/katex/auto-render.min.js',
+  '/markdown_previewer/js/mdpreview/marked.min.js',
+  '/markdown_previewer/js/mdpreview/preview.js',
+  '/markdown_previewer/js/pwa/pwa.js',
+  '/markdown_previewer/js/pwa/service-worker.js',
+  '/markdown_previewer/manifest.json'
 ];
 
 // Install event - Cache all specified files
